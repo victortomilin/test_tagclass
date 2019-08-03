faae0b4868e98c69d1047cd244048f40c145b096
Viktor Tomilin <viktortomilin@gmail.com>
2019-08-03 23:59:02 +0300

    Refactor a code

--- election/election.js
+++ election/election.js
@@ -1,33 +1,29 @@
+function countVotes (votes) {
+  const VOTE = 1
+  const counts = {}
+
+  for (let i = 0; i < votes.length; i++) {
+    const candidate = votes[i]
+    const amount = counts[candidate]
+    counts[candidate] = amount ? amount + VOTE : VOTE
+  };
+
+  return counts
+}
+
+const convertCountsToArray = counts => Object.values(counts)
+const sortVotesInDescendingOrder = numberOfVotes => numberOfVotes.sort((a, b) => b - a)
+const getMajorityScore = votes => Math.floor(votes.length / 2)
+
 exports.tally = function (votes) {
-  function countVotes () {
-    const VOTE = 1
-    const counts = {}
-
-    for (let i = 0; i < votes.length; i++) {
-      const candidate = votes[i]
-      const amount = counts[candidate]
-      counts[candidate] = amount ? amount + VOTE : VOTE
-    };
-
-    return counts
-  }
-
-  function convertCountsToArray (counts) {
-    return Object.values(counts)
-  }
-
-  function sortVotesInDescendingOrder (numberOfVotes) {
-    return numberOfVotes.sort(function (a, b) { return b - a })
-  }
-
-  const counts = countVotes()
+  const counts = countVotes(votes)
   const numberOfVotes = sortVotesInDescendingOrder(convertCountsToArray(counts))
-  const majorityScore = Math.floor(votes.length / 2)
-  const isWinnerReachesMajorityOfVotes = numberOfVotes[0] >= majorityScore
+  const winner = numberOfVotes[0]
+  const isWinnerReachesMajorityOfVotes = winner >= getMajorityScore(votes)
 
   function getMajorityWinner () {
     for (const key in counts) {
-      if (counts[key] === numberOfVotes[0]) {
+      if (counts[key] === winner) {
         return [Number(key)]
       }
     }
@@ -35,10 +31,10 @@ exports.tally = function (votes) {
 
   function getWinners () {
     const winners = []
-    const firstThreePositions = numberOfVotes.slice(0, 3)
+    const firstThreeWinners = numberOfVotes.slice(0, 3)
     for (const key in counts) {
       const value = counts[key]
-      const isWinner = firstThreePositions.includes(value)
+      const isWinner = firstThreeWinners.includes(value)
       if (isWinner) {
         winners.push(Number(key))
       }
